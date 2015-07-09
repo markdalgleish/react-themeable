4c08e107faa3dd16b089fa2fa452fb7b4feab9ed
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-07-10 07:53:22 +1000

    Add tests

---
+++ test/index.js
@@ -0,0 +1,81 @@
+import themeable from '../src';
+import { expect } from 'chai';
+
+describe('className', () => {
+  const classes = { foo: 'aaa', bar: 'bbb' };
+  const classTheme = themeable(classes);
+
+  it('should return a single class', () => {
+    expect(classTheme(1, 'foo'))
+      .to.deep.equal({
+        key: 1,
+        className: classes.foo
+      });
+  });
+
+  it('should return multiple classes', () => {
+    expect(classTheme(2, 'foo', 'bar'))
+      .to.deep.equal({
+        key: 2,
+        className: `${classes.foo} ${classes.bar}`
+      });
+  });
+
+  it('should ignore falsy values', () => {
+    expect(classTheme(1, null, 'foo', undefined, false))
+      .to.deep.equal({
+        key: 1,
+        className: classes.foo
+      });
+  });
+
+});
+
+describe('style', () => {
+  const styles = {
+    foo: {
+      color: 'red',
+      fontSize: '16px'
+    },
+    bar: {
+      color: 'blue',
+      fontWeight: 'bold'
+    }
+  };
+  const styleTheme = themeable(styles);
+
+  it('should return a single style', () => {
+    expect(styleTheme(1, 'foo'))
+      .to.deep.equal({
+        key: 1,
+        style: {
+          color: 'red',
+          fontSize: '16px'
+        }
+      });
+  });
+
+  it('should return multiple styles merged', () => {
+    expect(styleTheme(1, 'foo', 'bar'))
+      .to.deep.equal({
+        key: 1,
+        style: {
+          fontSize: '16px',
+          color: 'blue',
+          fontWeight: 'bold'
+        }
+      });
+  });
+
+  it('should ignore falsy values', () => {
+    expect(styleTheme(1, false, undefined, 'foo', null))
+      .to.deep.equal({
+        key: 1,
+        style: {
+          color: 'red',
+          fontSize: '16px'
+        }
+      });
+  });
+
+});
